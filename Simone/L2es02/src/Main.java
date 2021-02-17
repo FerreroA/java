9299a9b023ff96dbad907911f1db5abbb7d3579b
Alessandro Ferrero <ale.ferrero@icloud.com>
2021-02-17 13:09:03 +0100

    add folder Simone and update myWork

---
+++ Simone/L2es02/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    public static int contaVocali(String a) {
+        sum = 
+        return sum;
+    }
+}
